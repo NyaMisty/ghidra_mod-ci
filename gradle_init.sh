46907fb72e5374c24f00573d34694bcbe0699f1b
NyaMisty <misty@misty.moe>
2023-02-18 13:34:28 +0800

    Unify gradle init script

---
+++ gradle_init.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+ 
+  set -x
+  gradle --version
+  for i in {1..4}; do
+    gradle -PcurrentPlatformName=${{ matrix.target }} --init-script gradle/support/fetchDependencies.gradle init && break
+    rm -rf .gradle
+    #ps -ef | grep gradle | xargs kill -9
+    rm -rf dependencies
+    if command -v taskkill >/dev/null; then
+      (tasklist | grep -i '[g]radle' | awk '{print $2}' | xargs -n1 taskkill -f -pid) || true
+    else
+      (ps -ef | grep -i '[g]radle' | awk '{print $2}' | xargs -n1 kill -9) || true
+    fi
+  done
+
